d484ed9eacc00987be5ad3d3d5bb534570a79d48
pjwooda <jpatrickwoodall@gmail.com>
2020-06-24 10:25:58 -0400

    Additional update after saveing

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "example" {
 
   tag {
     key                 = "Name"
-    value               = "${var.cluster_name}-example"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
 
