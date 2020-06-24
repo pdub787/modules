0fe30433e97ae6814f3fcb5736a21a1f5bbd8722
pjwooda <jpatrickwoodall@gmail.com>
2020-06-24 12:13:18 -0400

    Another update to var.tf

--- services/webserver-cluster/var.tf
+++ services/webserver-cluster/var.tf
@@ -34,7 +34,7 @@ variable "max_size" {
   type        = number
 }
 
-bariable "custom_tags" {
+variable "custom_tags" {
   description = "Custom tags to set on the Instances in the ASG"
   type        = map(string)
   default     = {}
