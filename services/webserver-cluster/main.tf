de7de60fb35e2e34c2e3e159cb1ffcaf17acff80
pjwooda <jpatrickwoodall@gmail.com>
2020-07-01 15:24:15 -0400

    updated modules/main repo

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -3,11 +3,7 @@ resource "aws_launch_configuration" "example" {
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
-  user_data = (
-    length(data.template_file.user_data[*]) > 0
-      ? data.template_file.user_data[0].rendered
-      : data.template_file.user_data_new[0].rendered
-  )
+  user_data = data.template_file.user_data.rendered
 
   # Required when using a launch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
@@ -17,24 +13,13 @@ resource "aws_launch_configuration" "example" {
 }
 
 data "template_file" "user_data" {
-  count = var.enable_new_user_data ? 0 : 1
-
   template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
     db_address = data.terraform_remote_state.db.outputs.address
     db_port    = data.terraform_remote_state.db.outputs.port
-  }
-}
-
-data "template_file" "user_data_new" {
-  count = var.enable_new_user_data ? 1 : 0
-
-  template = file("${path.module}/user-data-new.sh")
-
-  vars = {
-    server_port = var.server_port
+    server_text = var.server_text
   }
 }
 
